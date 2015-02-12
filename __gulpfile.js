77b82a53435ca7d0136e3bbea8eca79d4a9cb992
Piotr Gabara <piotr.gabara@rulefinancial.com>
2015-02-12 11:20:54 +0100

    Polishing default, test and inject tasks

--- __gulpfile.js
+++ __gulpfile.js
@@ -9,8 +9,10 @@ var jshint = require('gulp-jshint');
 var inject = require('gulp-inject');
 var useref = require('gulp-useref');
 var nodemon = require('gulp-nodemon');
+var plumber = require('gulp-plumber');
 var minifyCss = require('gulp-minify-css');
 var minifyHtml = require('gulp-minify-html');
+var autoprefixer = require('gulp-autoprefixer');
 var templateCache = require('gulp-angular-templatecache');
 
 var args = require('yargs').argv;
@@ -27,7 +29,7 @@ var reload = browserSync.reload;
  * Remove temporary and production folders
  */
 gulp.task('clean', function (done) {
-	del(['./.tmp', './dist', './reports'], done);
+    del(['./.tmp', './dist', './report'], done);
 });
 
 
@@ -36,10 +38,10 @@ gulp.task('clean', function (done) {
  * Usage: gulp bump --type major|minor|patch or gulp bump for default 'patch' type
  */
 gulp.task('bump', function () {
-	return gulp.src(['./package.json', './bower.json'])
-		.pipe(print())
-		.pipe(bump({type: args.type || 'patch'}))
-		.pipe(gulp.dest('./'));
+    return gulp.src(['./package.json', './bower.json'])
+        .pipe(print())
+        .pipe(bump({type: args.type || 'patch'}))
+        .pipe(gulp.dest('./'));
 });
 
 
@@ -47,63 +49,42 @@ gulp.task('bump', function () {
  * Run nodemon and watch for changes
  */
 gulp.task('nodemon', function () {
-	return nodemon({
-		script: './src/server/app.js',
-		env: { 'NODE_ENV': 'dev', 'PORT': 7203 },
-		watch: ['src/server/**/*.js']
-	})
-	.on('restart', function (ev) {
-		console.log('Restarting nodemon ...');
-		console.log('Changed files:\n' + ev);
-	});
+    return nodemon({
+        script: './src/server/app.js',
+        env: { 'NODE_ENV': 'dev', 'PORT': 7203 },
+        watch: ['src/server/**/*.js']
+    })
+    .on('restart', function (ev) {
+        console.log('Restarting nodemon ...');
+        console.log('Changed files:\n' + ev);
+    });
 });
 
 
 /**
  * Run browser sync
  */
-gulp.task('browser-sync', ['nodemon'], function () {
-	if (browserSync.active) {
-		return;
-	}
+gulp.task('browser-sync', ['nodemon', 'inject'], function () {
+    if (browserSync.active) {
+        return;
+    }
 
-	browserSync({
-		proxy: 'localhost:7203',
-		browser: 'chrome',
-		injectChanges: true,
-		files: ['./src/client/**/*.html', './src/client/**/*.js', './.tmp/styles.css']
-	});
+    browserSync({
+        proxy: 'localhost:7203',
+        browser: 'chrome',
+        injectChanges: true,
+        files: ['./src/client/**/*.html', './src/client/**/*.js', './.tmp/styles.css']
+    });
 });
 
 /**
  * Lint all js files with JSHint and JSCS
  */
 gulp.task('lint', function () {
-	return gulp.src(['./src/**/*.js', '*.js'])
-		.pipe(jscs())
-		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish', {verbose: true}));
-});
-
-
-/**
- * Inject js and css files into index.html
- */
-gulp.task('inject', ['less'], function () {
-	return gulp.src('./src/client/index.html')
-		.pipe(inject(gulp.src(['./src/client/app/**/*.js', './src/client/app/**/*.module.js', '!./src/client/app/**/*.spec.js']), {read: false}))
-		.pipe(inject(gulp.src(['./.tmp/**/*.css']), {read: false}))
-		.pipe(gulp.dest('./src/client'));
-});
-
-
-/**
- * Inject bower js and css files into index.html
- */
-gulp.task('wiredep', function () {
-	return gulp.src('./src/client/index.html')
-	.pipe(wiredep({ignorePath: '../..', json: require('./bower.json'), directory: './bower_components/'}))
-		.pipe(gulp.dest('./src/client'));
+    return gulp.src(['./src/**/*.js', '*.js'])
+        .pipe(jscs())
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
 });
 
 
@@ -111,9 +92,32 @@ gulp.task('wiredep', function () {
  * Compile LESS to CSS
  */
 gulp.task('less', function () {
-	return gulp.src('./src/client/styles/**/*.less')
-		.pipe(less())
-		.pipe(gulp.dest('./.tmp/'));
+    return gulp.src('./src/client/styles/**/*.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(autoprefixer({browsers: ['last 2 version', '> 5%']}))
+    .pipe(gulp.dest('./.tmp/'));
+});
+
+
+/**
+ * Inject bower js and css files into index.html
+ */
+gulp.task('wiredep', function () {
+    return gulp.src('./src/client/index.html')
+    .pipe(wiredep({ignorePath: '../..', json: require('./bower.json'), directory: './bower_components/'}))
+    .pipe(inject(gulp.src(['./src/client/app/**/*.module.js', './src/client/app/**/*.js', '!./src/client/app/**/*.spec.js']), {read: false}))
+    .pipe(gulp.dest('./src/client'));
+});
+
+
+/**
+ * Inject js and css files into index.html
+ */
+gulp.task('inject', ['wiredep', 'less'], function () {
+    return gulp.src('./src/client/index.html')
+        .pipe(inject(gulp.src(['./.tmp/**/*.css']), {read: false}))
+        .pipe(gulp.dest('./src/client'));
 });
 
 
@@ -121,10 +125,19 @@ gulp.task('less', function () {
  * Cache all angular templates
  */
 gulp.task('template-cache', function () {
-	return gulp.src('./src/client/app/**/*.html')
-		.pipe(minifyHtml({empty: true}))
-		.pipe(templateCache({module: 'app.core'}))
-		.pipe(gulp.dest('./.tmp/'));
+    var options = {
+        file: 'templates.js',
+        options: {
+            module: 'app.core',
+            standalone: false,
+            root: 'app/'
+        }
+    };
+
+    return gulp.src('./src/client/app/**/*.html')
+        .pipe(minifyHtml({empty: true}))
+        .pipe(templateCache(options.file, options.options))
+        .pipe(gulp.dest('./.tmp/'));
 });
 
 
@@ -132,16 +145,16 @@ gulp.task('template-cache', function () {
  * Optimize css and js files
  */
 gulp.task('optimize', ['wiredep', 'inject'], function () {
-	var assets = useref.assets({searchPath: './'});
+    var assets = useref.assets({searchPath: './'});
 
-	return gulp.src('./src/client/index.html')
-		.pipe(inject(gulp.src('./.tmp/templates.js'), {read: false}), {starttag: '<!-- inject:templates:js -->'})
-		.pipe(assets)
-		.pipe(gulpif('*.js', uglify()))
-		.pipe(gulpif('*.css', minifyCss()))
-		.pipe(assets.restore())
-		.pipe(useref())
-		.pipe(gulp.dest('./dist'));
+    return gulp.src('./src/client/index.html')
+        .pipe(inject(gulp.src('./.tmp/templates.js'), {read: false}), {starttag: '<!-- inject:templates:js -->'})
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('./dist'));
 });
 
 
@@ -149,18 +162,17 @@ gulp.task('optimize', ['wiredep', 'inject'], function () {
  * Run tests
  */
 gulp.task('test', ['template-cache'], function (done) {
-	karma.start({
-		configFile: __dirname + '/karma.conf.js',
-		exclude: ['./src/client/tests/server-integration/**/*.spec.js'],
-		singleRun: false
-	}, done);
+    karma.start({
+        configFile: __dirname + '/karma.conf.js',
+        exclude: ['./src/client/tests/server-integration/**/*.spec.js'],
+        singleRun: false
+    }, done);
 });
 
 
 /**
  * Default task
  */
-gulp.task('default', ['inject', 'wiredep', 'browser-sync'], function () {
-	gulp.start('test');
-	gulp.watch(['./src/client/styles/**/*.less'], ['less']);
+gulp.task('default', ['browser-sync', 'test'], function () {
+    gulp.watch(['./src/client/styles/**/*.less'], ['less']);
 });
--- __karma.conf.js
+++ __karma.conf.js
@@ -1,70 +1,71 @@
 module.exports = function(config) {
-	var bowerDependencies = require('wiredep')({devDependencies: true})['js'];
+    var bowerDependencies = require('wiredep')({devDependencies: true})['js'];
 
-	config.set({
-		// base path that will be used to resolve all patterns (eg. files, exclude)
-		basePath: './',
+    config.set({
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: './',
 
-		// frameworks to use
-		// some available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['mocha', 'chai', 'sinon', 'chai-sinon'],
+        // frameworks to use
+        // some available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['mocha', 'chai', 'sinon', 'chai-sinon'],
 
-		// list of files / patterns to load in the browser
-		files: [].concat(
-			bowerDependencies,
-			['./src/client/test-helpers/**/*.js'],
-			['./src/client/app/**/*.module.js'],
-			['./src/client/app/**/*.js', './.tmp/templates.js']
-		),
+        // list of files / patterns to load in the browser
+        files: [].concat(
+            bowerDependencies,
+            ['./src/client/test-helpers/**/*.js'],
+            ['./src/client/app/**/*.module.js'],
+            ['./src/client/app/**/*.js'],
+            ['./.tmp/**/*.js']
+        ),
 
-		// list of files to exclude
-		exclude: [],
+        // list of files to exclude
+        exclude: [],
 
-		proxies: {
-			'/': 'http://localhost:8888/'
-		},
+        proxies: {
+            '/': 'http://localhost:8888/'
+        },
 
-		// preprocess matching files before serving them to the browser
-		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-		preprocessors: {
-			'./src/client/app/**/!(*.spec)+(.js)': ['coverage']
-		},
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            './src/client/app/**/!(*.spec)+(.js)': ['coverage']
+        },
 
-		// test results reporter to use
-		// possible values: 'dots', 'progress', 'coverage'
-		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress', 'coverage'],
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'coverage'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'coverage'],
 
-		coverageReporter: {
-			dir: './reports/coverage',
-			reporters: [
-				{type: 'html', subdir: 'report-html'},
-			   	{type: 'lcov', subdir: 'report-lcov'},
-			   	{type: 'text-summary'}
-			]
-		},
+        coverageReporter: {
+            dir: './report/coverage',
+            reporters: [
+                {type: 'html', subdir: 'report-html'},
+               	{type: 'lcov', subdir: 'report-lcov'},
+               	{type: 'text-summary'}
+            ]
+        },
 
-		// web server port
-		port: 9876,
+        // web server port
+        port: 9876,
 
-		// enable / disable colors in the output (reporters and logs)
-		colors: true,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR ||
-		// config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
+        // config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: true,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-		// start these browsers
-		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		//        browsers: ['Chrome', 'ChromeCanary', 'FirefoxAurora', 'Safari', 'PhantomJS'],
-		browsers: ['PhantomJS'],
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        //        browsers: ['Chrome', 'ChromeCanary', 'FirefoxAurora', 'Safari', 'PhantomJS'],
+        browsers: ['PhantomJS'],
 
-		// Continuous Integration mode
-		// if true, Karma captures browsers, runs the tests and exits
-		singleRun: false
-	});
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false
+    });
 };
